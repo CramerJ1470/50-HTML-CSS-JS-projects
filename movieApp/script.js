e93f6d32616bb9520239bf771b5f82bd0b422899
cramerj1470 <john@Cramerec.com>
2022-12-30 21:29:03 -0500

    finishing up class

--- movieApp/script.js
+++ movieApp/script.js
@@ -1,19 +1,16 @@
 // get API_KEY from themoviedb.org or in your .env
-
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&language=en-US&page=1`;
-
-console.log(API_URL);
-
+API_KEY = ":9a317dc8a7d6e39cd7b4328a1b6d65e5";
+const API_URL =
+	"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+	'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
-
-const PIXAR_SEARCH = `https://api.themoviedb.org/3/discover/movie?with_genres=16&api_key=${API_KEY}&language=en-US&pages`;
-
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-const main = document.getElementById("main");
 
+// Get initial movies
 getMovies(API_URL);
 
 async function getMovies(url) {
@@ -28,24 +25,21 @@ function showMovies(movies) {
 
 	movies.forEach((movie) => {
 		const { title, poster_path, vote_average, overview } = movie;
-		// console.log(movie);
+
 		const movieEl = document.createElement("div");
 		movieEl.classList.add("movie");
 
 		movieEl.innerHTML = `
-                <img src="${IMG_PATH + poster_path}"
-                    alt="${title}"/>
-                <div class="movie-info">
-                    <h3>${title}</h3>
-                    <span class="${getClassByRate(
-						vote_average
-					)}">${vote_average}</span>
-                </div>
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
             <div class="overview">
-            <h3>Overview</h3>
-                ${overview}
-            </div>`;
-
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `;
 		main.appendChild(movieEl);
 	});
 }
@@ -66,24 +60,10 @@ form.addEventListener("submit", (e) => {
 	const searchTerm = search.value;
 
 	if (searchTerm && searchTerm !== "") {
-		getMovies(SEARCH_URL + searchTerm);
+		getMovies(SEARCH_API + searchTerm);
 
 		search.value = "";
 	} else {
 		window.location.reload();
 	}
 });
-
-getMovies1(PIXAR_SEARCH);
-async function getMovies1(url) {
-	const res = await fetch(url);
-	const data = await res.json();
-
-	console.log(data.results);
-	data.results.map((m) => {
-		if (m.name === "Animation") {
-			const animationId = m.id;
-			console.log(`genre `, m, `has an id of `, m.id);
-		}
-	});
-}
